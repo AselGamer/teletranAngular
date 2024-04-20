ece2ab643f1d11956d47eb944e25692ad41929aa
AselGamer <aselfernandez@gmail.com>
2024-04-20 17:24:23 +0200

    se envian los frames de la webcam al servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,30 +5,45 @@ import { Socket } from 'ngx-socket-io';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: false,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+selector: 'app-root',
+standalone: false,
+templateUrl: './app.component.html',
+styleUrl: './app.component.css'
 })
 export class AppComponent {
-  @ViewChild('videoElement') videoElement: ElementRef;
-  private videoStream: MediaStream;
+@ViewChild('videoElement') videoElement: ElementRef;
+@ViewChild('canvasElement') canvas: ElementRef;
+private videoStream: MediaStream;
 
-  constructor(private socket: Socket) { }
-  
-  ngOnInit(): void {
-    this.startCapture();
-    this.socket.emit("chat_message", )
-  }
+constructor(private socket: Socket) { }
 
-  startCapture() : void {
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-      this.videoStream = stream;
-      this.videoElement.nativeElement.srcObject = stream;
-    })
-    .catch((error) => {
-      console.error('Error accessing webcam:', error);
-    });
-  }
+ngOnInit(): void {
+  this.startCapture();
+  this.socket.emit('chat_message', 'Test');
+}
+
+startCapture(): void {
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then((stream) => {
+    this.videoStream = stream;
+    this.videoElement.nativeElement.srcObject = stream;
+    const recorder: MediaRecorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+      console.log(this.convert());
+    }
+    recorder.start(500);
+  })
+  .catch((error) => {
+    console.error('Error accessing webcam:', error);
+  });
+}
+
+convert() {
+  let canvas = this.canvas.nativeElement as HTMLCanvasElement;
+  const video = this.videoElement.nativeElement;
+  canvas.width = 200;
+  canvas.height = 200;
+  canvas.getContext('2d').drawImage(video, 0, 0, 200,200);  
+  this.socket.emit('frame', canvas.toDataURL()); 
+}
 }
