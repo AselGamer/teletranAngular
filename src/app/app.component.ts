c3662cc13908e5ac90a0ab9656a7d469a5b71486
AselGamer <aselfernandez@gmail.com>
2024-04-20 12:46:40 +0200

    se coge el video de la webcam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Socket } from 'ngx-socket-io';
 
@@ -11,13 +11,24 @@ import { Socket } from 'ngx-socket-io';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('videoElement') videoElement: ElementRef;
+  private videoStream: MediaStream;
 
-  constructor(private socket: Socket)
-  {
-
-  }
+  constructor(private socket: Socket) { }
   
   ngOnInit(): void {
-    
+    this.startCapture();
+    this.socket.emit("chat_message", )
+  }
+
+  startCapture() : void {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+      this.videoStream = stream;
+      this.videoElement.nativeElement.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error('Error accessing webcam:', error);
+    });
   }
 }
