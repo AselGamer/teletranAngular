4691560aab8054d42cffa8111bdd6ce9e48ec134
AselGamer <aselfernandez@gmail.com>
2024-05-22 19:03:46 +0200

    intento de arreglo de error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { RouterOutlet } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
-const config: SocketIoConfig = { url: 'http://socketteletraan.duckdns.org:5000/', options: {} };
+const config: SocketIoConfig = { url: 'http://socketteletraan.duckdns.org:5000', options: {} };
 
 @NgModule({
   declarations: [AppComponent],
